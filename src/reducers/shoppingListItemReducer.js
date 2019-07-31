d61c311768c0df32b5ac4fb12ee5935004eb31cc
mbarrins <maija.barrins@gmail.com>
2019-07-31 18:01:26 +0100

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -3,12 +3,26 @@ export default function shoppingListItemReducer(
 		items: []
 	},
 	action
-) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+// ) {
+// 	switch (action.type) {
+// 		case 'INCREASE_COUNT':
+// 			return state.items.concat(state.items.length + 1);
 
-		default:
-			return state;
-	}
+// 		default:
+// 			return state;
+// 	}
+) {
+		console.log(action);
+		switch (action.type) {
+			case 'INCREASE_COUNT':
+				console.log('Current state.items length %s', state.items.length);
+				console.log('Updating state.items length to %s', state.items.length + 1);
+				return Object.assign({}, state, {
+					items: state.items.concat(state.items.length + 1)
+				});
+	 
+			default:
+				console.log('Initial state.items length: %s', state.items.length);
+				return state;
+		}
 }
